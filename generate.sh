ab7cf80d442d450cb9a44f47a4cabb8a60b37a5e
Nathan Fish <nfish@uwaterloo.ca>
2026-01-29 15:00:34 -0500

    fix re-append

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 # Pre-process data
 jq 'map({name, rarity}) | {ancestries: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Ancestries_raw.json > data/Ancestries.json
 
-jq 'map({name, rarity}) | {backgrounds: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Backgrounds_raw.json >> data/Backgrounds.json
+jq 'map({name, rarity}) | {backgrounds: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Backgrounds_raw.json > data/Backgrounds.json
 
 cat data/Ancestries.json data/Backgrounds.json | jq -s 'add' > data/Compiled.json
 
