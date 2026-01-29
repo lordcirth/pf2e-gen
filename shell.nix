898dc3fcc4e15391088e106996f3915eaab98d7a
Nathan Fish <lordcirth@gmail.com>
2026-01-29 11:01:50 -0500

    init blank python shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+# shell.nix
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2026-01-29 (25.11 stable). Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/fa83fd837f3098e3e678e6cf017b2b36102c7211.tar.gz") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      #pandas
+      #requests
+    ]))
+  ];
+}
