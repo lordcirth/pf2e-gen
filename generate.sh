bd42a35cad89e146d9a1ef508697a7c587e2c30c
Nathan Fish <nfish@uwaterloo.ca>
2026-01-29 13:31:43 -0500

    2-roll table

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 # Pre-process data
 jq 'map({name, rarity}) | {ancestries: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Ancestries_raw.json > data/Ancestries.json
 
-#jinja2 index.html.jinja <(jq '{ancestries: .}' data/Ancestries.json) --format=json
+jinja2 index.html.jinja data/Ancestries.json --format=json > index.html
