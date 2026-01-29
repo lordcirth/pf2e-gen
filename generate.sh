d16bc40a2121ae26987e3b3aaad8638293f426b7
Nathan Fish <nfish@uwaterloo.ca>
2026-01-29 14:42:46 -0500

    fix offset parameters

--- generate.sh
+++ generate.sh
@@ -1,4 +1,8 @@
 # Pre-process data
 jq 'map({name, rarity}) | {ancestries: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Ancestries_raw.json > data/Ancestries.json
 
-jinja2 index.html.jinja data/Ancestries.json --format=json > index.html
+jq 'map({name, rarity}) | {backgrounds: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Backgrounds_raw.json >> data/Backgrounds.json
+
+cat data/Ancestries.json data/Backgrounds.json | jq -s 'add' > data/Compiled.json
+
+jinja2 index.html.jinja data/Compiled.json  --format=json -o index.html
