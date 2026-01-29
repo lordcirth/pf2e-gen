49f14aa1d4788ef08192d9908ff42b5e792264f1
Nathan Fish <nfish@uwaterloo.ca>
2026-01-29 13:23:51 -0500

    Parse ancestries by rarity

--- generate.sh
+++ generate.sh
@@ -1,1 +1,4 @@
-jinja2 index.html.jinja <(jq '{ancestries: .}' data/Ancestries.json) --format=json
+# Pre-process data
+jq 'map({name, rarity}) | {ancestries: {common: map(select(.rarity == "Common")), uncommon: map(select(.rarity == "Uncommon")), rare: map(select(.rarity == "Rare"))}} ' < data/Ancestries_raw.json > data/Ancestries.json
+
+#jinja2 index.html.jinja <(jq '{ancestries: .}' data/Ancestries.json) --format=json
